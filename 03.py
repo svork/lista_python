67b2d6f10211808326b8879d879d9561001f7728
Rodrigo Costa <svork@users.noreply.github.com>
2018-10-13 12:44:00 -0300

    Exercício 03

---
+++ 03.py
@@ -0,0 +1,7 @@
+n0 = int(input("Digite um número: "))
+n1 = int(input("Digite um número: "))
+n2 = int(input("Digite um número: "))
+lista = [n0, n1, n2]
+lista.sort()
+lista.reverse()
+print(lista)
